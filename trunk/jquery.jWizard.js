7ac2ba0a5c1e23c7b110d548ce53d3f9ba9b6c5e
mako281 <mako281@f6f540b2-75bb-11de-ba67-499d525147dd>
2009-08-18 17:03:56 +0000

    Version 0.3a

--- trunk/jquery.jWizard.js
+++ trunk/jquery.jWizard.js
@@ -3,6 +3,7 @@
  * @author	Dominic Barnes
  * @desc	A wizard plugin that actually works with minimal configuration. (per jQuery's design philosophy)
  * @type	jQuery
+ * @version	0.3a
  *
  * --- Features ---
  * - Minimal HTML, CSS and JS startup requirements
@@ -10,199 +11,190 @@
  * - Stylable (via CSS Classes)
  * - Step-based Form Validation
  */
-(function($) {
-    $.fn.extend({
-        jWizard: function(settings, validation) {
-			/**
-			 * --- Default Parameters ---
-			 * @param hideCancelButton If set to true, this will hide the "Cancel" Button (defaults to false)
-			 * @param cssClasses Assign CSS Class Names to different components of the Wizard
-			 * @param onStepChange Custom Event: Fires off any time the Step changes (more specifically, when the "Next" or "Previous" button is clicked)
-			 * @param onCancel Custom Event: Fires off when the user clicks the "Cancel" button
-			 * @param onFinish Custom Event: Fires off when the user clicks the "Finish" button
-			 */
-			var defaults = {
-				enableValidation: false,
-				hideCancelButton: false,
-				hideTitle: false,
-				/* CSS Classes */
-				stepDivClass: 'step',
-				buttonsDivClass: 'buttons',
-				titleDivClass: 'title',
-				cancelButtonClass: 'wizardButton',
-				previousButtonClass: 'wizardButton',
-				nextButtonClass: 'wizardButton',
-				finishButtonClass: 'wizardButton',
-				/* Events */
-				onCancel: function() { alert('Cancel Button Clicked'); },
-				onStepChange: function() { return true; },
-				onValidateStep: function() { return true; },
-				onFinish: function() { alert('Finish Button Clicked'); }
-			};
+(function($){
+	var jWizard = function(element, options) {
+		/**
+		 * --- Default Parameters ---
+		 * @param hideCancelButton If set to true, this will hide the "Cancel" Button (defaults to false)
+		 * @param cssClasses Assign CSS Class Names to different components of the Wizard
+		 * @param onStepChange Custom Event: Fires off any time the Step changes (more specifically, when the "Next" or "Previous" button is clicked)
+		 * @param onCancel Custom Event: Fires off when the user clicks the "Cancel" button
+		 * @param onFinish Custom Event: Fires off when the user clicks the "Finish" button
+		 */
+		var defaults = {
+			activeStep: 0,
+			enableValidation: false,
+			hideCancelButton: false,
+			hideTitle: false,
 
-			/* Assign our Default Parameters (override with anything the end-user supplies) */
-			settings = $.extend({}, defaults, settings);
+			/* Button Text */
+			cancelButtonText: 'Cancel',
+			previousButtonText: 'Previous',
+			nextButtonText: 'Next',
+			finishButtonText: 'Finish',
 
-			return this.each(function() {
-				var w = $(this);	// Create a reference to the wizard
+			/* CSS Classes */
+			stepDivClass: 'step',
+			buttonsDivClass: 'buttons',
+			titleDivClass: 'title',
+			cancelButtonClass: 'wizardButton',
+			previousButtonClass: 'wizardButton',
+			nextButtonClass: 'wizardButton',
+			finishButtonClass: 'wizardButton',
 
-				w.selectors = {
-					steps: 'div.' + settings.stepDivClass,
-					buttonsDiv: 'div.' + settings.buttonsDivClass,
-					titleDiv: 'div.' + settings.titleDivClass,
-					currentStep: 'div.' + settings.stepDivClass + ':visible'
-				};
+			/* Events */
+			onCancel: function() { alert('Cancel Button Clicked'); },
+			onStepChange: function() { return true; },
+			onValidateStep: function() { return true; },
+			onFinish: function() { alert('Finish Button Clicked'); }
+		};
 
-				w.find('div').addClass('ui-widget-content').addClass(settings.stepDivClass);	// Add the assigned class to the Step <div>'s
+		/* Assign our Default Parameters (override with anything the end-user supplies) */
+		var options = $.extend({}, defaults, options);
 
-				// Create our Action <button>s and <div>
-				var titleDiv = $('<div />').addClass('ui-widget-header').addClass(settings.titleDivClass);
-				var buttonsDiv = $('<div />').addClass('ui-widget-content').addClass(settings.buttonsDivClass);
-				var cancelButton = $('<button />').attr('id', 'jw_cancel').addClass('ui-state-default').html('Cancel');
-				var previousButton = $('<button />').attr('id', 'jw_previous').addClass('ui-state-default').html('Previous');
-				var nextButton = $('<button />').attr('id', 'jw_next').addClass('ui-state-default').html('Next');
-				var finishButton = $('<button />').attr('id', 'jw_finish').addClass('ui-state-default').html('Finish');
-				buttonsDiv.append(cancelButton).append(previousButton).append(nextButton).append(finishButton);
-				w.append(buttonsDiv);
-				w.prepend(titleDiv);
+		var w = $(element);	// Create a reference to the wizard itself
 
-				w.find('.ui-state-default').hover(
-					function() { $(this).addClass('ui-state-hover'); },
-					function() { $(this).removeClass('ui-state-hover'); }
-				);
+		// Assign some strings here so we can easily call on them again (rather than running those crazy concats all the time)
+		var selectors = {
+			steps: 'div.' + options.stepDivClass,
+			buttonsDiv: 'div.' + options.buttonsDivClass,
+			titleDiv: 'div.' + options.titleDivClass,
+			currentStep: 'div.' + options.stepDivClass + ':visible'
+		};
 
-				/* Identify our newly created <button>s */
-				w.cancelButton = w.find('button#jw_cancel');
-				w.nextButton = w.find('button#jw_next');
-				w.previousButton = w.find('button#jw_previous');
-				w.finishButton = w.find('button#jw_finish');
+		// Create our Action <button>s and <div>
+		w.titleDiv = $('<div />').addClass('ui-widget-header').addClass(options.titleDivClass);
+		w.buttonsDiv = $('<div />').addClass('ui-widget-content').addClass(options.buttonsDivClass);
+		var cancelButton = $('<button />').attr('id', 'jw_cancel').addClass('ui-state-default').addClass(options.cancelButtonClass).html(options.cancelButtonText);
+		var previousButton = $('<button />').attr('id', 'jw_previous').addClass('ui-state-default').addClass(options.previousButtonClass).html(options.previousButtonText);
+		var nextButton = $('<button />').attr('id', 'jw_next').addClass('ui-state-default').addClass(options.nextButtonClass).html(options.nextButtonText);
+		var finishButton = $('<button />').attr('id', 'jw_finish').addClass('ui-state-default').addClass(options.finishButtonClass).html(options.finishButtonText);
+		w.buttonsDiv.append(cancelButton).append(previousButton).append(nextButton).append(finishButton);
 
-				/* Add the assigned classes to our <button>s */
-				w.cancelButton.addClass(settings.cancelButtonClass);
-				w.previousButton.addClass(settings.previousButtonClass);
-				w.nextButton.addClass(settings.nextButtonClass);
-				w.finishButton.addClass(settings.finishButtonClass);
+		/**
+		 * @member	"Next" Button Click Event
+		 * @desc	This moves us to the next step.
+		 */
+		nextButton.click(function() {
+			w.changeStep(w.currentStep.next(selectors.steps));
+		});
 
-				if (settings.hideCancelButton)	w.cancelButton.hide();	// Hide the Cancel Button if the settings specify to
+		/**
+		 * @member	"Previous" Button Click Event
+		 * @desc	This moves us to the previous step.
+		 */
+		previousButton.click(function() {
+			w.changeStep(w.currentStep.prev(selectors.steps));
+		});
 
-				w.find(w.selectors.steps + ':not(:first)').hide();	// Hide every step except the first one
-				w.previousButton.hide();	// The previous button is hidden by default
-				w.finishButton.hide();	// The finish button is hidden by default
+		/**
+		 * @member	"Cancel" Button Click Event
+		 * @desc	Call the User-Supplied onCancel method
+		 */
+		cancelButton.click(function() {
+			w.trigger('onCancel');
+		});
 
-				/* Identify what step is what */
-				w.allSteps = w.find(w.selectors.steps);
-				w.firstStep = w.find(w.selectors.steps + ':first');
-				w.lastStep = w.find(w.selectors.steps + ':last');
-				w.currentStep = w.firstStep;
+		/**
+		 * @member	"Finish" Button Click Event
+		 * @desc	Call the User-Supplied onFinish method
+		 */
+		finishButton.click(function() {
+			w.trigger('onFinish');
+		});
 
-				w.title = w.find(w.selectors.titleDiv).text(w.firstStep.attr('title'));
+		/**
+		 * @member changeStep
+		 * @desc
+		 */
+		w.changeStep = function(nextStep) {
+			if (!w.triggerHandler('onStepChange', [ w.currentStep, nextStep ]))	return false;	// If the trigger returns false, we return false here before the switch occurs
 
+			w.currentStep.hide();	// Hide the current Step
+			nextStep.show();
+			w.titleDiv.text(nextStep.attr('title'));
 
-				/**
-				 * @member	"Next" Button Click Event
-				 * @desc	This moves us to the next step.
-				 */
-				w.nextButton.click(function() {
-					if (settings.enableValidation) {
-						w.bind('onValidateStep', settings.onValidateStep);
-						if (!w.triggerHandler('onValidateStep'))
-							return false;
-					}
+			w.currentStep = w.find(selectors.currentStep);	// Update the current Step
+			w.updateButtons();
+		};
 
-					if (w.currentStep.attr('id') != w.lastStep.attr('id'))
-					{
-						var nextStep = w.currentStep.next(w.selectors.steps);
-						w.changeStep(nextStep);
-						w.changeButtons();
-					}
-				});
+		/**
+		 * @member	updateButtons
+		 * @desc	Based on the current step, we will show and hide certain buttons.
+		 */
+		w.updateButtons = function() {
+			var currentId = w.currentStep.attr('id');
+			var firstId = w.firstStep.attr('id');
+			var lastId = w.lastStep.attr('id');
 
-				/**
-				 * @member	"Previous" Button Click Event
-				 * @desc	This moves us to the previous step.
-				 */
-				w.previousButton.click(function() {
-					if (settings.enableValidation) {
-						w.bind('onValidateStep', settings.onValidateStep);
-						if (!w.trigger('onValidateStep'))
-							return false;
-					}
+			switch (currentId)
+			{
+				case firstId:
+					previousButton.hide();
+					nextButton.show();
+					finishButton.hide();
+					break;
 
-					if (w.currentStep.attr('id') != w.firstStep.attr('id'))
-					{
-						var previousStep = w.currentStep.prev(w.selectors.steps);
-						w.changeStep(previousStep);
-						w.changeButtons();
-					}
-				});
+				case lastId:
+					previousButton.show();
+					nextButton.hide();
+					finishButton.show();
+					break;
 
-				/**
-				 * @member	"Cancel" Button Click Event
-				 * @desc	Call the User-Supplied onCancel method
-				 */
-				w.cancelButton.click(function() {
-					w.bind('onCancel', settings.onCancel);
-					w.trigger('onCancel');
-				});
+				default:
+					previousButton.show();
+					nextButton.show();
+					finishButton.hide();
+					break;
+			}
+		};
 
-				/**
-				 * @member	"Finish" Button Click Event
-				 * @desc	Call the User-Supplied onFinish method
-				 */
-				w.finishButton.click(function() {
-					w.bind('onFinish', settings.onFinish);
-					w.trigger('onFinish');
-				});
+		w.goToStep = function(index) {
+			w.changeStep(w.find(selectors.steps + ':eq(' + index + ')'));
+		};
 
-				/**
-				 * @member changeStep
-				 * @desc
-				 */
-				w.changeStep = function(nextStep)
-				{
-					w.bind('onStepChange', { currentStep: w.currentStep, nextStep: nextStep }, settings.onStepChange);
-					if (!w.triggerHandler('onStepChange'))	return false;	// If the trigger returns false, we return false here before the switch occurs
+		w.bind('onFinish', options.onFinish);
+		w.bind('onCancel', options.onCancel);
+		w.bind('onStepChange', options.onStepChange);
+		// w.bind('onValidateStep', options.onValidateStep); --NOT YET IMPLEMENTED
 
-					w.currentStep.hide();	// Hide the current Step
-					nextStep.show();
-					w.title.text(nextStep.attr('title'));
+		w.children('div').addClass('ui-widget-content').addClass(options.stepDivClass);	// Add the assigned class to the Step <div>'s
 
-					w.currentStep = $(w.selectors.currentStep);	// Update the current Step
-				};
+		w.firstStep = w.find(selectors.steps + ':first');
+		w.lastStep = w.find(selectors.steps + ':last');
+		w.currentStep = w.find(selectors.steps + ':eq(' + options.activeStep + ')');
 
-				/**
-				 * @member	checkButtons
-				 * @desc	Based on the current step, we will show and hide certain buttons.
-				 */
-				w.changeButtons = function() {
-					var currentId = w.currentStep.attr('id');
-					var firstId = w.firstStep.attr('id');
-					var lastId = w.lastStep.attr('id');
+		w.prepend(w.titleDiv);
+		w.append(w.buttonsDiv);
 
-					switch (currentId)
-					{
-						case firstId:
-							w.previousButton.hide();
-							w.nextButton.show();
-							w.finishButton.hide();
-							break;
+		w.find('.ui-state-default').hover(
+			function() { $(this).addClass('ui-state-hover'); },
+			function() { $(this).removeClass('ui-state-hover'); }
+		);
 
-						case lastId:
-							w.previousButton.show();
-							w.nextButton.hide();
-							w.finishButton.show();
-							break;
+		w.find(selectors.steps).hide();
 
-						default:
-							w.previousButton.show();
-							w.nextButton.show();
-							w.finishButton.hide();
-							break;
-					}
-				};
+		if (options.hideCancelButton)	cancelButton.hide();	// Hide the Cancel Button if the options specify to
+		previousButton.hide();	// The previous button is hidden by default
+		finishButton.hide();	// The finish button is hidden by default
 
-				return w;
-            });
-        }
-    });
+		w.goToStep(options.activeStep);
+
+		return w;
+	};
+
+	$.fn.jWizard = function(options) {
+		return this.each(function() {
+			var element = $(this);
+
+			// Return early if this element already has a plugin instance
+			if (element.data('jWizard')) return;
+
+			// pass options to plugin constructor
+			var w = new jWizard(this, options);
+
+			// Store plugin object in this element's data
+			element.data('jWizard', w);
+		});
+	};
 })(jQuery);
